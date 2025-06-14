354a95a06e71a0a8499587e9ca365ba4ac2fdc0d
Gojer <gojer@naver.com>
2025-06-14 11:41:21 -0400

    Adding variants of the project section

--- src/utils/variants.js
+++ src/utils/variants.js
@@ -228,3 +228,47 @@ export const timelineCircleVariants = {
     }
   }
 };
+
+//variant for project section
+export const sectionVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1
+    }
+  }
+};
+
+export const headingVariants = {
+  hidden: { opacity: 0, y: -30 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, delay: 0.2 }
+  }
+};
+
+export const projectCardVariants = {
+  hidden: { opacity: 0, y: 50, scale: 0.9 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      damping: 15
+    }
+  }
+};
+
+export const fadeInGrow = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 0.4, ease: "easeOut" }
+  }
+};
