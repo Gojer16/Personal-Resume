cc3815094616efce9c82f18dd86be1d088120b0e
Gojer <gojer@naver.com>
2025-06-14 09:28:03 -0400

    Experience data

---
+++ src/utils/experienceData.js
@@ -0,0 +1,35 @@
+export const experienceData = [
+  {
+    id: 'hospital-it-support',
+    company: 'Hospital José María Vargas.',
+    location: 'Aragua, Cagua',
+    title: 'Intership IT Support',
+    startDate: 'Sep 2020',
+    endDate: 'Dec 2020',
+    description: [
+      'Developed and executed a comprehensive preventive maintenance plan for hospital computer systems.',
+    ],
+  },
+  {
+    id: 'eti-it-support',
+    company: 'Escuela Técnica Industrial Mariano Fernández Fortique.',
+    location: 'Aragua, Cagua',
+    title: 'Intership IT Support',
+    startDate: 'Jul 2023',
+    endDate: 'Dec 2023',
+    description: [
+      'Managed software/hardware compatibility, implemented preventive maintenance, and upgraded systems for enhanced operational stability.',
+    ],
+  },
+  {
+    id: 'eti-full-stack',
+    company: 'Escuela Técnica Industrial Mariano Fernández Fortique.',
+    location: 'Aragua, Cagua',
+    title: 'Intership Full-Stack Developer',
+    startDate: 'Jan 2025',
+    endDate: 'April 2025',
+    description: [
+      'Built a full-stack institutional website for a Technical school.',
+    ],
+  }
+];
